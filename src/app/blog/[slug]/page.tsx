9233b54d5f5ac31a8027fdcc0efcc77f1b73866e
Muhammad Hilal Soorty <hilal.soorty111@gmail.com>
2024-01-31 20:19:59 +0500

    changes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -27,15 +27,17 @@ return data
 
 
 export default async function blogArticle({params}:{params:{slug:string}}) {
+  
   const data:fullBlog = await getData(params.slug)
 
     return(
+      <>
       <div>
       <h1>
-        <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">
+        <span id="back-to-top" className="block text-base text-center text-primary font-semibold tracking-wide uppercase">
           Hilal Soorty - Blog
         </span>
-        <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl ">
+        <span  className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl ">
           {data.title}
           <div className="mt-4">
         <span className="text-sm text-gray-500">Tags:</span>
@@ -62,10 +64,25 @@ export default async function blogArticle({params}:{params:{slug:string}}) {
       />
       <div className="mt-16 prose prose-red prose-lg dark:prose-invert prose-li:marker:text-primary">
         <PortableText value={data.content} />
-      </div>
-    
-      
+      </div>    
     </div>
-    
+    <div className="flex justify-end fixed bottom-8 right-8">
+        <Link  className="text-red-500 hover:underline" href={`/blog/${data.currentSlug}/#back-to-top`} >
+<div className="flex">
+<div>
+ Back To Top
+      </div>
+        <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18" />
+</svg>
+        </div>
+  
+</div>
+        
+
+        </Link>
+      </div>
+    </>
     )
 } 
